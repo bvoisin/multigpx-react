a1d9af3bd7d6066d7b1f2fe7d34124fca0705616
Benoît Voisin <bvo1976@gmail.com>
2020-11-27 14:58:17 +0100

    Fetching files presigned URLs

--- pages/api/getList.ts
+++ pages/api/getList.ts
@@ -1,23 +1,41 @@
 import * as aws from 'aws-sdk';
 import {NextApiRequest, NextApiResponse} from 'next';
 
+export interface GpxFileInfo {
+    key: string;
+    url: string;
+}
+
+export type GpxFileList = GpxFileInfo[];
+
+async function listFiles$(): Promise<GpxFileList> {
+    return new Promise<GpxFileInfo[]>((resolve, reject) => {
+        aws.config.update({
+            accessKeyId: process.env.ACCESS_KEY,
+            secretAccessKey: process.env.SECRET_KEY,
+
+            region: process.env.REGION,
+            signatureVersion: 'v4',
+        });
+
+        const s3 = new aws.S3();
+        s3.listObjects({Delimiter: '/', Prefix: process.env.FILE_PREFIX, Bucket: process.env.BUCKET_NAME}, function (err, data) {
+            console.log('data ', {err, data})
+            const fileList$ = data.Contents
+                .map(f => f.Key)
+                .filter(key => key.endsWith('.gpx'))
+                .map(key =>
+                    s3.getSignedUrlPromise('getObject', {Bucket: process.env.BUCKET_NAME, Key: key})
+                        .then(url => ({key, url} as GpxFileInfo)))
+            resolve(Promise.all(fileList$))
+        });
+    })
+}
+
 export default async (_: NextApiRequest, res: NextApiResponse) => {
-
     // res.status(200).json(['a', 'b']);
+    const fileList = await listFiles$()
+    console.log('lst ', {fileList})
+    res.status(200).json(fileList);
 
-    aws.config.update({
-        accessKeyId: process.env.ACCESS_KEY,
-        secretAccessKey: process.env.SECRET_KEY,
-
-        region: process.env.REGION,
-        signatureVersion: 'v4',
-    });
-
-    const s3 = new aws.S3();
-    s3.listObjects({Delimiter: '/', Prefix: process.env.FILE_PREFIX, Bucket: process.env.BUCKET_NAME}, function (err, data) {
-        console.log('data ', {err, data})
-        const lst = data.Contents.map(f => f.Key).filter(n => n.endsWith('.gpx'));
-        console.log('lst ', {lst})
-        res.status(200).json(lst);
-    });
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import React from 'react';
 import useSWR from 'swr';
+import {GpxFileList} from '../pages/api/getList';
 
 export default function Home() {
     // async function getList() {
@@ -13,7 +14,7 @@ export default function Home() {
     //   return json;
     // }
 
-    const {data, error} = useSWR(`api/getList`)
+    const {data: fileList, error} = useSWR<GpxFileList>(`api/getList`)
 
     return (
         <Layout home>
@@ -21,7 +22,10 @@ export default function Home() {
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                Data: {data}
+                Data:
+                <ul>
+                    {fileList?.map(f => <li key={f.key}>{f.key} <a href={f.url}>Link</a></li>)}
+                </ul>
                 <br/>
                 Error: {error}
             </section>
