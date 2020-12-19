433f8d429e3aac4f405ec723dd8649c037d891f7
Benoît Voisin <bvo1976@gmail.com>
2020-12-19 23:00:52 +0100

    Animations

--- components/myMap.tsx
+++ components/myMap.tsx
@@ -1,6 +1,6 @@
 import {MapContainer, MapContainerProps} from 'react-leaflet';
 import React from 'react';
-import {LatLngBounds, Layer, Map as LeafletMap} from 'leaflet';
+import {LatLngBounds, Layer, Map as LeafletMap, Path} from 'leaflet';
 import {GpxFileRefs} from 'pages/api/getGpxFileList';
 import {createLeafletGpx} from 'lib/3rdParty/leafletgpx';
 import {parseToGpxFileInfo2} from 'lib/gpx/parseToGpxFileInfo';
@@ -10,38 +10,47 @@ import MyLayerControl from 'components/myMapLayerControl';
 import {Subject} from 'rxjs';
 import {debounceTime, scan} from 'rxjs/operators';
 
+// const colors = [
+//     '#7c7c7c',
+//     '#d34f00',
+//     '#b96500',
+//     '#a57000',
+//     '#937800',
+//     '#817f00',
+//     '#6c8500',
+//     '#4f8a00',
+//     '#009004',
+//     '#008f42',
+//     '#008e5d',
+//     '#008d6f',
+//     '#008c7d',
+//     '#008b89',
+//     '#008a94',
+//     '#00899f',
+//     '#0088aa',
+//     '#0086b7',
+//     '#0084c7',
+//     '#0081dc',
+//     '#007afe',
+//     '#606fff',
+//     '#8f5dff',
+//     '#bc3afc',
+//     '#db00dd',
+//     '#e400b5',
+//     '#e90096',
+//     '#ed007a',
+//     '#f0005e',
+//     '#f2003f',
+//     '#f30011'
+// ]
+
 const colors = [
-    '#7c7c7c',
-    '#d34f00',
-    '#b96500',
-    '#a57000',
-    '#937800',
-    '#817f00',
-    '#6c8500',
-    '#4f8a00',
-    '#009004',
-    '#008f42',
-    '#008e5d',
-    '#008d6f',
-    '#008c7d',
-    '#008b89',
-    '#008a94',
-    '#00899f',
-    '#0088aa',
-    '#0086b7',
-    '#0084c7',
-    '#0081dc',
-    '#007afe',
-    '#606fff',
-    '#8f5dff',
-    '#bc3afc',
-    '#db00dd',
-    '#e400b5',
-    '#e90096',
-    '#ed007a',
-    '#f0005e',
-    '#f2003f',
-    '#f30011'
+    '#FFFF00',
+    '#00FFFF',
+    '#FF00FF',
+    '#00FF00',
+    '#0000FF',
+    '#FF0000'
 ]
 
 let colorIndex = 0;
@@ -87,6 +96,14 @@ export default function MyMap(opts: MyMapContainerProps) {
         lgpx['on']('loaded', function (e) {
             const gpx = e.target;
             boundsRequest$.next(e.target.getBounds())
+
+            const tracePath: Path = gpx.getLayers()[0] as Path;
+            const traceEl = tracePath.getElement();
+            traceEl.classList.add('flashingTrace')
+            const delayOffset = Math.floor(Math.random() * 8);
+            traceEl.classList.add('flashingTrace_delay_'+delayOffset)
+            const durationOffset = Math.floor(Math.random() * 8);
+            traceEl.classList.add('flashingTrace_duration_'+durationOffset)
 
             gpx.on('click', () => {
                 showFile(gpxFile);
