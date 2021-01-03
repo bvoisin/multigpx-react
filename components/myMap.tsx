8d2f4ef8d4cab22492a933c11dbdc15a502f9b13
Benoît Voisin <bvo1976@gmail.com>
2021-01-03 14:50:13 +0100

    With GpxTraceLoader

--- components/myMap.tsx
+++ components/myMap.tsx
@@ -1,89 +1,60 @@
 import {MapContainer, MapContainerProps} from 'react-leaflet';
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Map as LeafletMap} from 'leaflet';
-import {GpxFileRefs} from 'pages/api/getGpxFileList';
 import {MainPageContext} from 'lib/mainPageContext';
 import MyLayerControl from 'components/myMapLayerControl';
 import {GpxFileInfo} from 'lib/gpx/gpxFileInfo';
-import {parseToGpxFileInfo2} from 'lib/gpx/parseToGpxFileInfo';
 import GpxTrace from 'components/GpxTrace';
 import FlyToSupport from 'components/FlyToSupport';
+import GpxTraceLoader from 'components/GpxTraceLoader';
 
 export interface MyMapContainerProps extends MapContainerProps {
 
 }
 
 export interface MyMapContainerState {
-    fileList: GpxFileInfo[]
+    loadedGpxFiles: GpxFileInfo[]
 }
 
 // export default function MyMap({children, position, zoom = 10, whenCreated}: { children?: any, position: LatLngExpression, zoom: number, whenCreated?: (map: LeafletMap) => void }) {
 export default function MyMap(opts: MyMapContainerProps) {
 
-    const [{fileList}, setState] = useState<MyMapContainerState>({fileList: []});
+    const [{loadedGpxFiles}, setState] = useState<MyMapContainerState>({loadedGpxFiles: []});
+    const {fileDirectory, displayMode, flyToCommand$} = useContext(MainPageContext)
 
-    return (
-        <MainPageContext.Consumer>
-            {({fileDirectory, displayMode, flyToCommand$, flyToRequest}) => {
+    function fillMap(map: LeafletMap) {
+        // console.log('fillMap', {map});
+        // TODO followInNewGpxFiles(map);
 
-                async function addGpxsToMap(map: LeafletMap, gpxFileList: GpxFileRefs) {
-                    const promises = gpxFileList.map((gpxFileRef) => {
-                        return parseToGpxFileInfo2(gpxFileRef);
-                    });
-                    return Promise.all(promises).then(lst => setState(s => {
-                        return ({fileList: [...s.fileList, ...lst]});
-                    }))
-                }
+        map.on('moveend', event => {
+            console.log('moveend ', {event, bounds: map.getBounds()});
+        })
+    }
 
-                function loadGpxTraces(map: LeafletMap, dir: string) {
-                    fetch(`api/getGpxFileList?directory=${dir}`)
-                        .then(res => res.json())
-                        .then(gpxFileRefs => {
-                                return addGpxsToMap(map, gpxFileRefs as GpxFileRefs)
-                            }
-                        );
-                }
+    function addTraceToMap(f: GpxFileInfo) {
+        setState(({loadedGpxFiles}) => ({loadedGpxFiles: [...loadedGpxFiles, f]}))
+    }
 
-                function showJoyeuxNoel(map: LeafletMap) {
-                    console.log('showJoyeuxNoel');
-                    flyToRequest(null, {duration: 10, easeLinearity: 0.1}, false);
-                    loadGpxTraces(map, '_joyeux-noel');
-                }
+    function removeTracesFromMap(files: (GpxFileInfo | string)[]) {
+        const fileNames = new Set(files.map(f => typeof f === 'string' ? f : f.fileName));
+        setState(({loadedGpxFiles}) => ({loadedGpxFiles: [...loadedGpxFiles.filter(f => !fileNames.has(f.fileName))]}))
+    }
 
-                function fillMap(map: LeafletMap) {
-                    // console.log('fillMap', {map});
-                    // TODO followInNewGpxFiles(map);
-
-                    loadGpxTraces(map, fileDirectory);
-
-                    //TODO fireFlashing();
-
-                    map.on('moveend', event => {
-                        console.log('moveend ', {event, bounds: map.getBounds()});
-                    })
-
-                    if (displayMode == 'xmas2') {
-                        setTimeout(() => showJoyeuxNoel(map), 5000)
-                    }
-                }
-
-                const mapOpts: MapContainerProps = {
-                    ...opts, whenCreated: map => {
-                        if (opts.whenCreated) {
-                            opts.whenCreated(map)
-                        }
-                        fillMap(map);
-                    },
-                }
-                console.log('State ', fileList);
-                return <MapContainer {...mapOpts}>
-                    <MyLayerControl displayMode={displayMode}/>
-                    {fileList.map(file => {
-                        return <GpxTrace gpxFileInfo={file} key={file.fileName} flashPeriodFactor={Math.sqrt(fileList.length)}/>;
-                    })}
-                    <FlyToSupport flyToCommand$={flyToCommand$}/>
-                </MapContainer>;
-            }}
-        </MainPageContext.Consumer>
-    );
+    const mapOpts: MapContainerProps = {
+        ...opts, whenCreated: map => {
+            if (opts.whenCreated) {
+                opts.whenCreated(map)
+            }
+            fillMap(map);
+        },
+    }
+    console.log('State ', loadedGpxFiles);
+    return <MapContainer {...mapOpts}>
+        <MyLayerControl displayMode={displayMode}/>
+        {loadedGpxFiles.map(file => {
+            return <GpxTrace gpxFileInfo={file} key={file.fileName} flashPeriodFactor={Math.sqrt(loadedGpxFiles.length)}/>;
+        })}
+        <GpxTraceLoader directory={fileDirectory} addTraceToMapCb={addTraceToMap} removeTracesFromMapCb={removeTracesFromMap}/>
+        <FlyToSupport flyToCommand$={flyToCommand$}/>
+    </MapContainer>;
 }
